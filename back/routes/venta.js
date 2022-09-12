0d0cd3f12ab589c9fa0c710fccd70f5cad84bb90
luaolayare <luaolayare@unal.edu.co>
2022-09-12 18:29:38 -0500

    ActualizacionProyecto

--- back/routes/venta.js
+++ back/routes/venta.js
@@ -10,3 +10,4 @@ api.post('/registro_compra_cliente',auth.auth,ventaController.registro_compra_cl
 api.get('/enviar_correo_compra_cliente/:id',auth.auth,ventaController.enviar_correo_compra_cliente);
 
 module.exports = api;
+
